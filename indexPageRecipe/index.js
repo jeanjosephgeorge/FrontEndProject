38f4cbbe1d619bbd241e25ce39a33382d079c775
hirosoft40 <hirosoft40@gmail.com>
2019-01-02 09:10:04 -0600

    merge master

---
+++ indexPageRecipe/index.js
@@ -0,0 +1,49 @@
+$(function(){
+    $('.fa-plus').click(function(e){
+        $("#mainInput").fadeToggle();
+        $("#mainInput").focus();
+    });
+
+    $("input[type='text']").keypress(function (e) { 
+        if(e.which === 13){
+            let task = $(this).val();
+            // make li element
+            let li = $("<li>",{
+                "class":"list-item",
+                "text": task
+            });
+            let span = $("<span>",{"class":"trash"});
+            let i = $('<i>',{"class":"fas fa-trash-alt"});
+            span.append(i);
+            li.prepend(span);
+            $('ul').append(li);
+            $('input').val('');
+        }
+    });
+
+    $('#submitButton').on('click',function(){
+        let ingredients = '';
+        for (var j=0; j<(('#mainField ul li').length);j++){
+            var li = $('#mainField ul li')[j];
+            if (li !== undefined){
+                var text = li.innerText;
+                ingredients += text;
+                ingredients += ',';
+                // window.location='https:www.yahoo.com';
+                // window.location.pathname = '/new'
+        }
+    }
+    ingredients = ingredients.slice(0,-1);
+    console.log(ingredients);
+    });
+
+    $('#mainUl').on('click','li',function(){
+        $(this).toggleClass('completed');
+    });
+
+    $('#mainUl').on('click','.trash',function(){
+        $(this).parent().fadeOut(400,function(){
+            $(this).remove();
+        })
+    });
+});
